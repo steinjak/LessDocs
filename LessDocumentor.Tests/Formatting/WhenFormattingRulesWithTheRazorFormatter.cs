8b6e261254c6399ddf94632f22084d3f270d411e
Stein Jakob Nordbø <steinjak@gmail.com>
2012-11-09 20:42:28 +0100

    Added razor formatting support

---
+++ LessDocumentor.Tests/Formatting/WhenFormattingRulesWithTheRazorFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+namespace LessDocs.Tests.Formatting
+{
+    [TestFixture]
+    public class WhenFormattingRulesWithTheRazorFormatter
+    {
+        private string result;
+        private IEnumerable<DocumentedRule> rules;
+
+        [SetUp]
+        public void Given()
+        {
+            var extractor = new RuleExtractor();
+            rules = extractor.ExtractRules("LessFiles\\ValidNonImporting.less");
+            var formatter = new RazorFormatter("Formatting\\TestTemplate.cshtml");
+            result = formatter.FormatDocumentation(rules);
+        }
+
+        [Test]
+        public void TheRulesAreFormattedUsingTheSuppliedTemplate()
+        {
+            Assert.That(result, Is.EqualTo("Rules:\r\n" + string.Join("\r\n", rules.Select(r => r.Name)) + "\r\n"));
+        }
+    }
+}
---
+++ LessDocumentor/RazorFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace LessDocs
+{
+    public class RazorFormatter
+    {
+        private readonly string templatePath;
+
+        public RazorFormatter(string templatePath)
+        {
+            this.templatePath = templatePath;
+        }
+
+        public string FormatDocumentation(IEnumerable<DocumentedRule> documentedRules)
+        {
+            var template = File.ReadAllText(templatePath);
+            return RazorEngine.Razor.Parse(template, documentedRules);
+        }
+    }
+}
